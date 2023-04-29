66107f326910422c63bb7a4ec570dd2d6144fb7a
Rddrigo Cabrito <rodri.cabrito@gmail.com>
2023-04-29 18:00:08 +0100

    US3 - funcionalidades de listar seguidores, listar users a seguir e listar os itens da biblioteca

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
+import { Game_search_DTO } from '../games/game_search_DTO';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,16 +10,39 @@ import { UserService } from '../user.service';
 })
 export class DashboardComponent implements OnInit {
   users: User[] = [];
+  followers: User[] = [];
+  following: User[] = [];
+  games: Game_search_DTO[] = [];
+  lists: String[] = []; //TODO lists type & getter
   showAppC = false;
+  id: string = ''; //TODO get self id
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
     this.getUsers();
+    this.getFollowers(this.id);
+    this.getFollowing(this.id);
+    this.getGamesLibrary(this.id);
   }
 
   getUsers(): void {
     this.userService.getUsers()
       .subscribe(users => this.users = users.slice(1, 5));
+  }
+
+  getFollowers(id: string): void {
+    this.userService.getUserFollowers(id)
+      .subscribe(followers => this.followers = followers);
+  }
+
+  getFollowing(id: string): void {
+    this.userService.getUserFollowing(id)
+      .subscribe(following => this.following = following);
+  }
+
+  getGamesLibrary(id: string): void {
+    this.userService.getGamesLibrary(id)
+    .subscribe(games => this.games = games);;
   }
 }
--- frontend/src/app/user.ts
+++ frontend/src/app/user.ts
@@ -1,7 +1,11 @@
 import { Pet } from "./pet";
+import { Game_search_DTO } from "./games/game_search_DTO";
 
 export interface User {
   id: String;
   name: string;
+  followers: User[];
+  following: User[];
+  games: Game_search_DTO[];
   petname: Pet;
 }
