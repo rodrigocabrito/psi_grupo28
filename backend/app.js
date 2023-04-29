80f89e6a8f6b72ce964bd32d0bb78d180931ed98
joaocosta <fc54482@alunos.fc.ul.pt>
2023-04-29 10:52:00 +0100

    Preparaçao para uso do appserver

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+var os = require('os');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -11,13 +12,13 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 const mongoose = require("mongoose");
-mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://1243613534:dGwal3uKkFX7KIzm@cluster0.ivd3p2m.mongodb.net/local_library?retryWrites=true&w=majority";
-
-main().catch(err => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB);
+var mongoDB = 'mongodb://127.0.0.1/psi028';
+if (os.hostname() == "appserver.alunos.di.fc.ul.pt") {
+  mongoDB = 'mongodb://psi028:psi028@localhost:27017/psi028?retryWrites=true&authSource=psi028';
 }
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
