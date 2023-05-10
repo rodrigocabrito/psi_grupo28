153c7d4f5ffac40b0e17fa76364b70579521263b
FineRocco <114879525+FineRocco@users.noreply.github.com>
2023-05-10 19:22:02 +0100

    Update edit-profile.component.ts

--- frontend/src/app/edit-profile/edit-profile.component.ts
+++ frontend/src/app/edit-profile/edit-profile.component.ts
@@ -1,56 +1,21 @@
 
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../user.service';
-import { Game_search_DTO } from '../games/game_search_DTO';
-import { User } from '../user';
-import { ActivatedRoute } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent implements OnInit {
+export class EditProfileComponent {
 
-    user: User | undefined;
-    followers: User[] = [];
-    following: User[] = [];
-    games: Game_search_DTO[] = [];
-    lists: String[] = []; //TODO lists type & getter
-    showAppC = false;
-    id: string = '';
-  
-    constructor(private userService: UserService, private route: ActivatedRoute) {}
-  
-    ngOnInit(): void {
-      this.getUser();
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
     }
-  
-    getFollowers(id: string): void {
-      this.userService.getUserFollowers(id)
-        .subscribe(followers => this.followers = followers);
-    }
-  
-    getFollowing(id: string): void {
-      this.userService.getUserFollowing(id)
-        .subscribe(following => this.following = following);
-    }
-  
-    getGamesLibrary(id: string): void {
-      this.userService.getGamesLibrary(id)
-      .subscribe(games => this.games = games);;
-    }
-  
-    getWishlish(id: string): void {
-      
-    }
-  
-    getUser(): void {
-      const session = window.localStorage.getItem("session");
-    if (session) {
-      this.id = JSON.parse(session);
-    }
-      this.userService.getUser(id)
-        .subscribe(user => this.user = user);
-    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
