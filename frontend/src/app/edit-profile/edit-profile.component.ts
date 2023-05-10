a70b2f3bd0715570852bf9d6995ac0a6aa121442
FineRocco <114879525+FineRocco@users.noreply.github.com>
2023-05-10 17:07:20 +0100

    Add files via upload

---
+++ frontend/src/app/edit-profile/edit-profile.component.ts
@@ -0,0 +1,53 @@
+
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { Game_search_DTO } from '../games/game_search_DTO';
+import { User } from '../user';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-profile',
+  templateUrl: './edit-profile.component.html',
+  styleUrls: ['./edit-profile.component.css']
+})
+export class EditProfileComponent implements OnInit {
+
+    user: User | undefined;
+    followers: User[] = [];
+    following: User[] = [];
+    games: Game_search_DTO[] = [];
+    lists: String[] = []; //TODO lists type & getter
+    showAppC = false;
+    id: string = '';
+  
+    constructor(private userService: UserService, private route: ActivatedRoute) {}
+  
+    ngOnInit(): void {
+      this.getUser();
+    }
+  
+    getFollowers(id: string): void {
+      this.userService.getUserFollowers(id)
+        .subscribe(followers => this.followers = followers);
+    }
+  
+    getFollowing(id: string): void {
+      this.userService.getUserFollowing(id)
+        .subscribe(following => this.following = following);
+    }
+  
+    getGamesLibrary(id: string): void {
+      this.userService.getGamesLibrary(id)
+      .subscribe(games => this.games = games);;
+    }
+  
+    getWishlish(id: string): void {
+      
+    }
+  
+    getUser(): void {
+      const id = this.route.snapshot.paramMap.get('id')!;
+      this.userService.getUser(id)
+        .subscribe(user => this.user = user);
+    }
+}
