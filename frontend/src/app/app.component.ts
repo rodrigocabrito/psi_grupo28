2b8abc30973d76cc0c98919a86fc32ac2a09fdf5
joaocosta <fc54482@alunos.fc.ul.pt>
2023-05-13 13:55:47 +0100

    cart em todo o lado

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router} from '@angular/router';
+import { Game_cart } from './games/game_cart';
+import { GameService } from './game_service/game.service';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +11,27 @@ import { Router} from '@angular/router';
 export class AppComponent {
   title = 'Oasis Gaming';
   id ="";
+  cart: Game_cart[] = [];
+  
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,private gameService: GameService) {}
 
   ngOnInit(): void {
     const session = window.localStorage.getItem("session");
     if (session) {
       this.id = JSON.parse(session);
     }
+    this.getCart();
+    
   }
 
   // Add a method to check the current route
   isRoute(route: string): boolean {
     return this.router.url.includes(route);
+  }
+
+  getCart(): void{
+    this.gameService.getCart(this.id)
+      .subscribe(cart => this.cart = cart);
   }
 }
